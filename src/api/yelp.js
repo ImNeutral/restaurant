762ccd57799e88643a5b61cc55dd63d8ff8ff976
Christian Garillo <garillo.cl@gmail.com>
2021-01-17 10:35:07 +0800

    Removed Bearer key/API Key to commit.

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 0o7A-Wh0f5hHGHzir3rVrw9hurxEUqFbPDZ90Wrhk9wr9cqJX_2MDkwwI3lYi_-coX3EMyVUOT4n9phXep5IJmn-uL8Re9PrpSh74lD1fXVndQtGek8kq2BrV_ECYHYx',
+        Authorization: '',
     }
 });
