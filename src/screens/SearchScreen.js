c12e91cc45016af6669a1ea99392be1731267b20
Christian Garillo <garillo.cl@gmail.com>
2021-01-17 10:58:08 +0800

    Initial search now working.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 5,
+                    term: searchTerm,
+                    location: 'Pasig City',
+                }
+            });
+            setResults(response.data.businesses);                
+        } catch (error) {
+            setErrorMessage('Something went wrong.');
+        }
+    };
+
+    useEffect(() => {
+        searchApi('rice');
+    }, [])
+
     return (
     <View>
         <SearchBar 
             term={term} 
-            onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log("submitted")}/>
-        <Text>Search Screen {term}</Text>
+            onTermChange={setTerm}
+            onTermSubmit={() => searchApi(term)}/>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text>We have found {results.length}</Text>
     </View>
     );
 }
